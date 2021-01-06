99e82efe4397cbc4ecdf466f33d696a93f2d6aeb
Siddharth Sharma <siddharthsharmajp@gmail.com>
2021-01-06 23:35:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!Doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+       	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 </body>
